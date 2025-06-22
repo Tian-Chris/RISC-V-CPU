691e098ca8af4a6d58ed1ec5f98c3f737a262d97
ChrisTian <tianchri@usc.edu>
2025-06-22 15:07:38 -0700

    inst exception

---
+++ Sources/decoder.v
@@ -0,0 +1,136 @@
+`timescale 1ns / 1ps
+
+module decoder (
+    input  wire        clk,
+    input  wire        rst,
+    input  wire        stall,
+    input  wire [1:0]  flushOut,
+    input  wire [31:0] instruction,
+    input  wire [31:0] pc,
+    input  wire [4:0]  rs1,
+    input  wire [4:0]  rs2,
+    input  wire [4:0]  rd,
+    output wire [31:0] IDinstruct_o,
+    output wire [31:0] IDPC_o,
+    output wire [4:0]  IDrs1_o,
+    output wire [4:0]  IDrs2_o,
+    output wire [4:0]  IDrd_o,
+    output wire [31:0]  IDinstCSR_o,
+    output wire        invalid_inst
+);
+`include "inst_defs.v"
+`include "csr_defs.v"
+
+reg [31:0] IDinstruct;
+reg [31:0] IDPC;
+reg [4:0]  IDrs1;
+reg [4:0]  IDrs2;
+reg [4:0]  IDrd;
+reg [31:0]  IDinstCSR;
+    
+always @(posedge clk) begin
+    `ifdef DEBUG
+        $display(" ");
+        $display("PC: %h", pc);
+        $display("Invalid Instruction: %b, IDins: %h, IDPC: %h, IDrs1, %h, IDrd: %h, IDinstCSR: %h", invalid_inst, IDinstruct, IDPC, IDrs1, IDrd, IDinstCSR);
+    `endif
+    if(rst) begin
+        IDinstruct <= `INST_NOP;
+        IDPC       <= 32'h00000000;
+        IDrs1      <= 5'b0;              
+        IDrs2      <= 5'b0;
+        IDrd       <= 5'b0; 
+        IDinstCSR  <= `INST_NOP;   
+    end
+    else if (!(stall)) begin
+        if (flushOut == 2'b11) begin
+            IDinstruct  <= `INST_NOP;
+            IDrs1       <= 5'b0;            
+            IDrs2       <= 5'b0;
+            IDrd        <= 5'b0;
+            IDinstCSR  <= 32'b0;                     
+        end
+        else if (flushOut == 2'b01)
+        begin
+            IDinstruct <= `INST_NOP;
+            IDrs1      <= 5'b0;               
+            IDrs2      <= 5'b0;
+            IDrd       <= 5'b0;   
+            IDinstCSR  <= 32'b0;           
+        end
+        else if(instruction[6:0] == 7'b1110011) begin
+            IDinstruct <= instruction;
+            IDPC       <= pc;
+            IDrs1      <= rs1;              
+            IDrs2      <= 5'b0;
+            IDrd       <= rd;              
+            IDinstCSR  <= instruction;
+        end
+        else begin
+            IDinstruct <= instruction;
+            IDPC       <= pc;
+            IDrs1      <= rs1;
+            IDrs2      <= rs2;
+            IDrd       <= rd;
+            IDinstCSR  <= 32'b0;
+        end
+    end 
+    else begin
+        IDinstruct <= `INST_NOP;
+        IDrs1      <= 5'b0;              
+        IDrs2      <= 5'b0;
+        IDrd       <= 5'b0;  
+        IDinstCSR  <= 32'b0;            
+    end
+end
+    assign IDinstruct_o = IDinstruct;
+    assign IDPC_o = IDPC;
+    assign IDrs1_o = IDrs1;
+    assign IDrs2_o = IDrs2;
+    assign IDrd_o = IDrd;
+    assign IDinstCSR_o = IDinstCSR;
+    assign invalid_inst = ~((IDinstruct  == `INST_NOP)    ||
+        ((IDinstruct & `INST_ADD_MASK)   == `INST_ADD)    ||
+        ((IDinstruct & `INST_SUB_MASK)   == `INST_SUB)    ||
+        ((IDinstruct & `INST_AND_MASK)   == `INST_AND)    ||
+        ((IDinstruct & `INST_OR_MASK)    == `INST_OR)     ||
+        ((IDinstruct & `INST_XOR_MASK)   == `INST_XOR)    ||
+        ((IDinstruct & `INST_SLL_MASK)   == `INST_SLL)    ||
+        ((IDinstruct & `INST_SRL_MASK)   == `INST_SRL)    ||
+        ((IDinstruct & `INST_SRA_MASK)   == `INST_SRA)    ||
+        ((IDinstruct & `INST_SLT_MASK)   == `INST_SLT)    ||
+        ((IDinstruct & `INST_SLTU_MASK)  == `INST_SLTU)   ||
+
+        ((IDinstruct & `INST_ANDI_MASK)  == `INST_ANDI)   ||
+        ((IDinstruct & `INST_ADDI_MASK)  == `INST_ADDI)   ||
+        ((IDinstruct & `INST_SLTI_MASK)  == `INST_SLTI)   ||
+        ((IDinstruct & `INST_SLTIU_MASK) == `INST_SLTIU)  ||
+        ((IDinstruct & `INST_ORI_MASK)   == `INST_ORI)    ||
+        ((IDinstruct & `INST_XORI_MASK)  == `INST_XORI)   ||
+
+        ((IDinstruct & `INST_SLLI_MASK)  == `INST_SLLI)   ||
+        ((IDinstruct & `INST_SRLI_MASK)  == `INST_SRLI)   ||
+        ((IDinstruct & `INST_SRAI_MASK)  == `INST_SRAI)   ||
+
+        ((IDinstruct & `INST_LB_MASK)    == `INST_LB)     ||
+        ((IDinstruct & `INST_LBU_MASK)   == `INST_LBU)    ||
+        ((IDinstruct & `INST_LH_MASK)    == `INST_LH)     ||
+        ((IDinstruct & `INST_LHU_MASK)   == `INST_LHU)    ||
+        ((IDinstruct & `INST_LW_MASK)    == `INST_LW)     ||
+
+        ((IDinstruct & `INST_SB_MASK)    == `INST_SB)     ||
+        ((IDinstruct & `INST_SH_MASK)    == `INST_SH)     ||
+        ((IDinstruct & `INST_SW_MASK)    == `INST_SW)     ||
+
+        ((IDinstruct & `INST_BEQ_MASK)   == `INST_BEQ)    ||
+        ((IDinstruct & `INST_BNE_MASK)   == `INST_BNE)    ||
+        ((IDinstruct & `INST_BLT_MASK)   == `INST_BLT)    ||
+        ((IDinstruct & `INST_BGE_MASK)   == `INST_BGE)    ||
+        ((IDinstruct & `INST_BLTU_MASK)  == `INST_BLTU)   ||
+        ((IDinstruct & `INST_BGEU_MASK)  == `INST_BGEU)   ||
+
+        ((IDinstruct & `INST_JAL_MASK)   == `INST_JAL)    ||
+        ((IDinstruct & `INST_JALR_MASK)  == `INST_JALR)   ||
+        ((IDinstruct & `INST_LUI_MASK)   == `INST_LUI)    ||
+        ((IDinstruct & `INST_AUIPC_MASK) == `INST_AUIPC));
+endmodule
---
+++ Sources/inst_defs.v
@@ -0,0 +1,104 @@
+
+//==============
+// Instructions
+//==============
+`define INST_NOP        32'h00000013
+
+// R-Type
+`define R_MASK          32'hfe00707f
+
+`define INST_ADD        32'h33
+`define INST_ADD_MASK   `R_MASK
+`define INST_SUB        32'h40000033
+`define INST_SUB_MASK   `R_MASK
+`define INST_AND        32'h7033
+`define INST_AND_MASK   `R_MASK
+`define INST_OR         32'h6033
+`define INST_OR_MASK    `R_MASK
+`define INST_XOR        32'h4033
+`define INST_XOR_MASK   `R_MASK
+`define INST_SLL        32'h1033
+`define INST_SLL_MASK   `R_MASK
+`define INST_SRL        32'h5033
+`define INST_SRL_MASK   `R_MASK
+`define INST_SRA        32'h40005033
+`define INST_SRA_MASK   `R_MASK
+`define INST_SLT        32'h2033
+`define INST_SLT_MASK   `R_MASK
+`define INST_SLTU       32'h3033
+`define INST_SLTU_MASK  `R_MASK
+
+// I-Type
+`define I_MASK          32'h707f
+
+`define INST_ANDI       32'h7013
+`define INST_ANDI_MASK  `I_MASK
+`define INST_ADDI       32'h13
+`define INST_ADDI_MASK  `I_MASK
+`define INST_SLTI       32'h2013
+`define INST_SLTI_MASK  `I_MASK
+`define INST_SLTIU      32'h3013
+`define INST_SLTIU_MASK `I_MASK
+`define INST_ORI        32'h6013
+`define INST_ORI_MASK   `I_MASK
+`define INST_XORI       32'h4013
+`define INST_XORI_MASK  `I_MASK
+
+// I*_Type
+`define ISTAR_MASK      32'hfe00707f
+
+`define INST_SLLI       32'h1013
+`define INST_SLLI_MASK  `ISTAR_MASK
+`define INST_SRLI       32'h5013
+`define INST_SRLI_MASK  `ISTAR_MASK
+`define INST_SRAI       32'h40005013
+`define INST_SRAI_MASK  `ISTAR_MASK
+
+// S_Type
+`define S_MASK          32'h707f
+
+`define INST_LB         32'h3
+`define INST_LB_MASK    `I_MASK
+`define INST_LBU        32'h4003
+`define INST_LBU_MASK   `I_MASK
+`define INST_LH         32'h1003
+`define INST_LH_MASK    `I_MASK
+`define INST_LHU        32'h5003
+`define INST_LHU_MASK   `I_MASK
+`define INST_LW         32'h2003
+`define INST_LW_MASK    `I_MASK
+`define INST_SB         32'h23
+`define INST_SB_MASK    `S_MASK
+`define INST_SH          32'h1023
+`define INST_SH_MASK    `S_MASK
+`define INST_SW         32'h2023
+`define INST_SW_MASK    `S_MASK
+
+//B_Type        
+`define B_MASK          32'h707f
+
+`define INST_BEQ        32'h63
+`define INST_BEQ_MASK   `B_MASK
+`define INST_BNE        32'h1063
+`define INST_BNE_MASK   `B_MASK
+`define INST_BLT        32'h4063
+`define INST_BLT_MASK   `B_MASK
+`define INST_BGE        32'h5063
+`define INST_BGE_MASK   `B_MASK
+`define INST_BLTU       32'h6063
+`define INST_BLTU_MASK  `B_MASK
+`define INST_BGEU       32'h7063
+`define INST_BGEU_MASK  `B_MASK
+
+// J_type and U_Type
+`define J_MASK          32'h7f
+`define U_MASK          32'h7f
+
+`define INST_JAL        32'h6f
+`define INST_JAL_MASK   `J_MASK
+`define INST_JALR       32'h67
+`define INST_JALR_MASK  `I_MASK
+`define INST_LUI        32'h37
+`define INST_LUI_MASK   `U_MASK
+`define INST_AUIPC      32'h17
+`define INST_AUIPC_MASK `U_MASK
